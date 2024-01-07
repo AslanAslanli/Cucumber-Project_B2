639846170b7a00f5f4430ae93ece34290c58857b
AslanAslanli <143632955+AslanAslanli@users.noreply.github.com>
2024-01-08 03:36:10 +0400

    commit all files

--- src/test/java/com/loop/runner/CukesRunner.java
+++ src/test/java/com/loop/runner/CukesRunner.java
@@ -5,10 +5,16 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions (
+        plugin = {"html:target/html-reports/cucumber-report.html",
+                  "json:target/json-reports/json-report.json"},
         features = "src/test/resources/features",
-        glue = "com/loop/step_definitions"
+        glue = "com/loop/step_definitions",
+        dryRun = false, // true dryRun checks snippet coverage for each scenario steps. dryRun false checks it AND everything inside the steps i.e. function bodies
+        tags = "@smoke",
+        monochrome = true // this removes redundant logs in the run terminal
 )
+
 public class CukesRunner {
 
 }
--- src/test/java/com/loop/step_definitions/LoginStepDefs.java
+++ src/test/java/com/loop/step_definitions/LoginStepDefs.java
@@ -1,34 +1,41 @@
 package com.loop.step_definitions;
-import io.cucumber.java.en.And;
+import com.loop.pages.LoginPage;
+import com.loop.utilities.BrowserUtil;
+import com.loop.utilities.ConfigurationReader;
+import com.loop.utilities.DocuportConstants;
+import com.loop.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static com.loop.utilities.BrowserUtil.waitForClickable;
+import static com.loop.utilities.DocuportConstants.USERNAME_CLIENT;
+import static com.loop.utilities.DocuportConstants.large;
+import static org.junit.Assert.assertTrue;
+
 public class LoginStepDefs {
-
+    LoginPage loginPage = new LoginPage();
+    //LoginPage loginPage = new LoginPage();
     @Given("user is on Docuport login page")
-    public void aslanIsCool() {
-        System.out.println("this is given step");
-    }
+    public void user_is_on_docuport_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
 
+    }
     @When("user enters username for client")
     public void user_enters_username_for_client() {
-        System.out.println("this is when");
+        BrowserUtil.waitForClickable(loginPage.usernameInput, DocuportConstants.large).sendKeys(DocuportConstants.USERNAME_CLIENT);
     }
-
     @When("user enters password for client")
     public void user_enters_password_for_client() {
-        System.out.println("this is when/and");
-    }
 
+    }
     @When("user clicks login buttom")
     public void user_clicks_login_buttom() {
-        System.out.println("this is when/and");
-    }
 
+    }
     @Then("user should see the home page for client")
     public void user_should_see_the_home_page_for_client() {
-        System.out.println("this is then");
+
     }
 
     @When("user enters username for employee")
@@ -41,7 +48,34 @@ public class LoginStepDefs {
     }
     @Then("user should see the home page for employee")
     public void user_should_see_the_home_page_for_employee() {
+;
+    }
+
+    @When("user enters username for advisor")
+    public void user_enters_username_for_advisor() {
 
     }
+    @When("user enters password for advisor")
+    public void user_enters_password_for_advisor() {
+
+    }
+    @Then("user should see the home page for advisor")
+    public void user_should_see_the_home_page_for_advisor() {
+
+    }
+
+    @When("user enters username for supervisor")
+    public void user_enters_username_for_supervisor() {
+
+    }
+    @When("user enters password for supervisor")
+    public void user_enters_password_for_supervisor() {
+
+    }
+    @Then("user should see the home page for supervisor")
+    public void user_should_see_the_home_page_for_supervisor() {
+
+    }
+
 
 }
